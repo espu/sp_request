b1deec3297d1fcaf354ad2b94970720019a354ae
espu <112239289+espu@users.noreply.github.com>
2023-01-17 15:51:10 +0200

    First commit

---
+++ request/src/main/java/spring23/request/web/RequestController.java
@@ -0,0 +1,19 @@
+package spring23.request.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class RequestController {
+	@RequestMapping("/index")
+	public String index() {
+		return "This is the main page";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact() {
+		return "This is the contact page";
+	}
+}
